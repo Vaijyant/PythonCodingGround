fa8d3884dc991061e10e3c294235f73f03db4455
Vaijyant Tomar <vaijyant.tomar@gmail.com>
2018-09-13 00:05:15 -0400

    Inheritance

--- 03_class.py
+++ 03_class.py
@@ -7,4 +7,14 @@ person_1 = Person("Vaijyant", 24)
 print(person_1.name)
 
 
+class Teacher(Person):
+    def __init__(self, name, age, subject):
+        Person.__init__(self, name, age)
+        self.subject = subject
 
+
+teacher_English = Teacher("James", 34, "English")
+
+print(teacher_English.name)
+print(teacher_English.age)
+print(teacher_English.subject)
